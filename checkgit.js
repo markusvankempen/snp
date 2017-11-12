9709c50daaae98368ce762b82aa3aac435a5693c
Markus van Kempen <markusvankempen43@gmail.com>
2017-11-12 18:59:14 -0500

    Update checkgit.js

--- checkgit.js
+++ checkgit.js
@@ -4,7 +4,8 @@ var exec = require('child_process').exec;
         console.log('pulling code from GitHub... mvk20171112 v1');
   // now pull down the latest
         exec('git -C /home/pi/gittest/snp pull -f', execCallback);
-
+  //
+        exec('sudo systemctl status  gitupdater.service', execCallback);
 
 function execCallback(err, stdout, stderr) {
         if(stdout) console.log(stdout);
